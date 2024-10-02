36d689f5ed8652c2047efb772de8d0ac3087c22a
third8bates <jessethieme@gmail.com>
2024-10-01 21:29:48 -0400

    Changes through page 26

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function getTitle(title) {
+  return title;
+}
+
+function App() {
+
+  return (
+      <div>
+        <h1>Hello {getTitle('React')}</h1>
+
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+      </div>
+  );
+}
+
+export default App;
