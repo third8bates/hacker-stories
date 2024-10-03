09e19cf43280ee79476a1595e825483640a637f0
third8bates <jessethieme@gmail.com>
2024-10-02 20:28:03 -0400

    Changes through page 32

--- src/App.jsx
+++ src/App.jsx
@@ -18,23 +18,33 @@ const list = [
 ];
 
 function App() {
-
   return (
-      <div>
-        <h1>My Hacker Stories</h1>
+    <div>
+      <h1>My Hacker Stories</h1>
 
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
 
-        <hr />
+      <hr />
 
+      <ul>
         <ul>
           {list.map(function (item) {
-            return <li key={item.objectID}>{item.title}</li>
+            return (
+              <li key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </li>
+            );
           })}
         </ul>
-      </div>
+      </ul>
+    </div>
   );
 }
 
-export default App;
+export default Ap
