3fb80200a56ecae52f38262b16906f601649cba7
third8bates <jessethieme@gmail.com>
2024-10-07 19:00:04 -0400

    Changes p. 48

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,21 @@ const App = () => (
 );
 
 //declaration of Search component
-const Search = () => (
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
-);
+  );
+}
 
 //declaration of List component
 const List = () => (
