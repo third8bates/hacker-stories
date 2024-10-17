9ddc720ce515f04fcce04a333e102a751665a729
third8bates <jessethieme@gmail.com>
2024-10-17 19:18:31 -0400

    p. 96 Reusable React Component

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch}/>
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
@@ -61,21 +66,24 @@ const App = () => {
   );
 };
 
-//declaration of Search component
-const Search = ({ search, onSearch }) => (
+const InputWithLabel = ({ 
+  id, 
+  label, 
+  value, 
+  type = 'text',
+  onInputChange,
+ }) => (
   <>
-    <label htmlFor="search">Search: </label>
-    <input 
-      id="search" 
-      type="text"
-      value={search}
-      onChange={onSearch}
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
+    <input
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
     />
   </>
 );
-
-// Variarion 2: Spread and Rest Operators
-// 2. Step
 
 //declaration of List component
 const List = ({ list }) => (
