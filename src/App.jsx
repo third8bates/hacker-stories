247da2d3bf841fd7978596a29b4e77226fc59691
third8bates <jessethieme@gmail.com>
2024-10-18 09:36:36 -0400

    p. 120 React Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -50,11 +50,18 @@ const App = () => {
   );
 
   const[stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    getAsyncStories().then(result => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+
+    getAsyncStories()
+      .then((result => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+    })
+      .catch(() => setIsError));
   }, []);
 
   const handleRemoveStory = (item) => {
@@ -73,6 +80,12 @@ const App = () => {
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  /* REMOVE?
+  if (isLoading) {
+    return <p>Loading...</p>
+  } 
+  */
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -88,7 +101,16 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong...</p>}
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List 
+          list={searchedStories} 
+          onRemoveItem={handleRemoveStory} 
+        />
+      )}
     </div>
   );
 };
