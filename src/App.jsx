1f2aabbf516ccf3f2fa298bebc4078d3c2d4c23a
third8bates <jessethieme@gmail.com>
2024-10-24 12:45:29 -0400

    p.154 Forms in React

--- src/App.jsx
+++ src/App.jsx
@@ -108,22 +108,11 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <form onSubmit={handleSearchSubmit}>
-        <InputWithLabel
-          id="search"
-          value={searchTerm}
-          isFocused
-          onInputChange={handleSearchInput}
-        >
-          <strong>Search:</strong>
-        </InputWithLabel>
-
-        <button
-        type="submit"
-        disabled={!searchTerm}>
-          Submit
-        </button>
-      </form>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
 
@@ -140,6 +129,26 @@ const App = () => {
     </div>
   );
 };
+
+const SearchForm = ({
+  searchTerm,
+    onSearchInput,
+    onSearchSubmit,
+  }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}>
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({ 
   id, 
