972f0c6726d38371b7cd31bc958ffffb2ade3604
third8bates <jessethieme@gmail.com>
2024-10-29 17:12:15 -0400

    p.178 Styled Componenets in React (not working)

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,90 @@
 import * as React from 'react';
 import axios from 'axios';
 //import clsx from 'clsx';
-import styles from './App.module.css';
+//import styles from './App.module.css';
+import styled from 'styled-components';
 
 // somewhere in a className attribute //
 // className={clsx(styles.button, { [styles.buttonLarge] isLarge })}
+
+const StyledContainer = styled.div`
+  height: 100vw;
+  padding: 20px;
+
+  background #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+  
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weigh: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.li`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  tect-overflow: ellipsis;
+
+  a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+  
+  &:hover {
+  background: #171212;
+  color: #ffffff;
+}
+`;
+
+const StyledButtonSmall = styled(styledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(styledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -139,7 +219,7 @@ const SearchForm = ({
     onSearchInput,
     onSearchSubmit,
   }) => (
-  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
+  <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -148,12 +228,11 @@ const SearchForm = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button type="submit" 
-      disabled={!searchTerm}
-      className={`${styles.button} ${styles.buttonLarge}`}>
+    <StyledButtonLarge type="submit" 
+      disabled={!searchTerm}>
         Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+  </StyledSearchForm>
 );
 
 const InputWithLabel = ({ 
@@ -174,11 +253,11 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id} className={styles.label}>
+      <StyledLabel htmlFor={id}>
         {children}
-      </label>
+      </StyledLabel>
       &nbsp;
-      <input
+      <StyledInput
         ref={inputRef}
         id={id}
         type={type}
@@ -203,22 +282,22 @@ const List = ({ list, onRemoveItem }) => (
 );
 
 const Item = ({ item, onRemoveItem }) => (
-    <li className={styles.item}>
-      <span style={{ width: '40%' }}>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span style={{ width: '30%' }}>{item.author}</span>
-      <span style={{ width: '10%' }}>{item.num_comments}</span>
-      <span style={{ width: '10%' }}>{item.points}</span>
-      <span style={{ width: '10%' }}>
-        <button 
-          type="button" 
-          onClick={() => onRemoveItem(item)}
-          className={`${styles.button} ${styles.buttonSmall}`}>
-            Dismiss
-        </button>
-      </span>
-    </li>
-  );
+  <StyledItem>
+    <StyledColumn width="40%">
+      <a href={item.url}>{item.title}</a>
+    </StyledColumn>
+    <StyledColumn width="30%"{item.author}</StyledColumn>
+    <StyledColumn width="10%"{item.num_comments}</StyledColumn>
+    <StyledColumn width="10%"{item.points}</StyledColumn>
+    <StyledColumn width="10%">
+      <StyledButtonSmall>
+        type="button" 
+        onClick={() => onRemoveItem(item)}
+      >
+        Dismiss
+      </StyledButtonSmall>
+    </StyledColumn>
+  </StyledItem>
+ );
 
 export default App;
