c253e5db453b7194d47fdefc95acc5184d11ef24
third8bates <jessethieme@gmail.com>
2024-11-10 14:32:34 -0500

    p. 215 Testing

--- src/App.jsx
+++ src/App.jsx
@@ -334,3 +334,5 @@ const Item = ({ item, onRemoveItem }) => (
  );
 
 export default App;
+
+export { storiesReducer, SearchForm, InputWithLabel, List, Item };
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,56 @@
 import { describe, it, expect } from 'vitest';
 
+import App, {
+    storiesReducer,
+    Item,
+    List,
+    SearchForm,
+    InputWithLabel,
+} from './App';
+
+const storyOne = {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+};
+
+const storyTwo = {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+};
+
+const stories = [storyOne, storyTwo];
+
+describe('storiesreducer', () => {
+    it('removes a story from all stories', () => {
+        const action = { type: 'REMOVE_STORY', payload: storyOne };
+        const state = { data: stories, isLoading: false, isError: false };
+
+        const newState = storiesReducer(state, action);
+
+        const expectedState = {
+            data: [storyTwo],
+            isLoading: false,
+        isError: false,
+        };
+
+        expect(newState).toStrictEqual(expectedState);
+    });
+});
+
 describe('something truthy and falsy', () => {
     it('true to be true', () => {
-        expect(true).toBe(true);
+        expect(true).toBeTruthy();
     });
 
     it('false to be false', () => {
-        expect(false).toBe(false);
+        expect(false).toBeFalsy();
     });
 });
